cfa3d0b77c564ac39fd6ebe35eb204455a6aff64
ModPunchtree <modpunchtree@gmail.com>
2022-12-06 06:19:26 +0000

    Copied python3.10 replit files.

--- replit.nix
+++ replit.nix
@@ -1,5 +1,18 @@
 { pkgs }: {
-	deps = [
-		pkgs.python310
-	];
+  deps = [
+    pkgs.python3
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONBIN = "${pkgs.python310}/bin/python3.10";
+    LANG = "en_US.UTF-8";
+  };
 }
